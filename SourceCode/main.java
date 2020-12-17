6cbcb84275b348760bccbeba6681f8d84fc41385
katherinesmirnov <54304561+katherinesmirnov@users.noreply.github.com>
2020-12-17 11:58:45 -0700

    Update main.java

--- SourceCode/main.java
+++ SourceCode/main.java
@@ -1,1 +1,14 @@
-
+int[] : grid
+draw out board
+display()
+for level hard
+  for (int i =0, i<20,i++){
+    random number generator for grid.unusedSpot
+    grid.fillBox()
+  }
+if input grid.isOkay()
+  keep box white
+  else
+    fill box red
+ 
+    
